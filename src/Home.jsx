3a2bfbd0c22399a65ec40e9ad8526c951135244a
ChurchillV <vinchurch70@gmail.com>
2023-06-06 19:42:24 +0000

    Handling click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,12 @@
 const Home = () => {
+    const handleClick = (event) => {
+        console.log("Hello Vince", event.target);
+    }
+
     return (
         <div className="home">
             <h1>Home</h1>
+            <button onClick={() => handleClick(event)}>Click Me</button>
         </div> 
      );
 }
