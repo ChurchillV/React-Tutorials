e6355885b19c01d845baeafd4772e422ff0eebc2
ChurchillV <vinchurch70@gmail.com>
2023-06-10 22:04:42 +0000

    useState Hooks, Devtools, Toggling on click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
-    const handleClick = (event) => {
-        console.log("Hello Vince", event.target);
+    const [name, setName] = useState('Vince');
+    let toggle = true;
+    const handleClick = () => {
+        if (toggle) {
+            setName('Keyshia');
+            toggle = false;
+            console.log(toggle);
+        }
+        else {
+            setName('Vince');
+            toggle = true;
+            console.log(toggle);
+        }
     }
 
     return (
         <div className="home">
             <h1>Home</h1>
+            <p>Hello { name }! Welcome to Sybil's store</p>
+            <button onClick={handleClick}>Click Me</button>
         </div> 
      );
 }
